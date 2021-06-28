5cbd041c0b002da313daef63c63e8d1887542ef3
Ilya Trofimov <ilya.v.trofimov@gmail.com>
2021-06-28 15:17:47 +1000

    Adding iam role

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "local" {}
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 3.47.0"
     }
   }
@@ -14,4 +14,28 @@ provider "aws" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
+}
+
+resource "aws_iam_role" "terraform-lab-role" {
+  name = "terraform-lab-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "terraform-lab-role-ec2access-attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  role = aws_iam_role.terraform-lab-role.name
 }
